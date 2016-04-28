3c49227c53d03ceeaabf0277881a1bd9f9e2a60f
Rui Wang <rui.x.wang@oracle.com>
2016-04-28 16:43:13 +0800

    Add rocketmq docker

--- kafka/run.sh
+++ kafka/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bin/zookeeper-server-start.sh config/zookeeper.properties
-bin/zookeeper-server-start.sh config/server.properties
+bin/zookeeper-server-start.sh config/zookeeper.properties&
+bin/kafka-server-start.sh config/server.properties&
